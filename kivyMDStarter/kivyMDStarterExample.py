2fb5b18b2d136ec611b1ff58249344d132476e1f
Alec McClure <blurmcclure16@gmail.com>
2022-01-10 10:01:33 -0600

    added kivyMD example file

---
+++ kivyMDStarter/kivyMDStarterExample.py
@@ -0,0 +1,20 @@
+from kivymd.uix.screen import MDScreen
+from kivymd.app import MDApp
+from kivy.uix.image import Image
+from kivymd.uix.button import MDFillRoundFlatIconButton, MDFillRoundFlatButton
+from kivymd.uix.textfield import MDTextField
+from kivymd.uix.label import MDLabel
+from kivymd.uix.toolbar import MDToolbar
+
+
+class MyApp(MDApp):
+    def build(self):
+        screen = MDScreen()
+
+        # UI Widgets go here
+
+        return screen
+
+
+if __name__ == "__main__":
+    MyApp().run()
